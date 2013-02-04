eaf4a742e16cbd9ceec71a98a19af2be0ea96338
Kelly Black <kjblack@gmail.com>
2013-02-04 12:46:42 -0500

    Made changes to the section on dealing with discrete data.

--- R/week1-Day2.R
+++ R/week1-Day2.R
@@ -2,7 +2,7 @@
 # Notes for week 1 day 2.
 
 # Define a function to make a Pareto Chart
-pareto = function(d,sortOrder,sortAtAll)
+pareto = function(d,sortOrder,sortAtAll,theTitle)
   {
     d = table(d)
     
@@ -17,7 +17,7 @@ pareto = function(d,sortOrder,sortAtAll)
     
     b = barplot(freq,xlab='Numbers',ylab='Frequency',
       ylim=c(0,1.1*sum(freq)),names=names(freq),
-      main='Pareto Diagram of the Data')
+      main=theTitle)
     lines(b,cumsum(freq))
     points(b,cumsum(freq))
     
@@ -29,17 +29,44 @@ d = c('B','B','B','A','A','C','D')
 
 
 png(file='paretoQuizW1D2-a.png')
-pareto(d,FALSE,TRUE)
+pareto(d,FALSE,TRUE,'Pareto Diagram of the Data')
 dev.off()
 
 
 png(file='paretoQuizW1D2-b.png')
-pareto(d,TRUE,TRUE)
+pareto(d,TRUE,TRUE,'Pareto Diagram of the Data')
 dev.off()
 
 png(file='paretoQuizW1D2-c.png')
-pareto(d,FALSE,FALSE)
+pareto(d,FALSE,FALSE,'Pareto Diagram of the Data')
 dev.off()
 
 
+
+
+res = c(
+  "Health Care",
+  "Health Care",
+  "Photo",
+  "Candy",
+  "Health Care",
+  "Cosmetics",
+  "Health Care",
+  "Candy",
+  "Health Care",
+  "Cosmetics",
+  "Cosmetics",
+  "Health Care",
+  "Photo"
+)
+
+f = table(res)
+
+png(file='drugStoreBarPlot.png')
+barplot(f,ylab="Number of Sales",main="Drug Store Visits by Department")
+dev.off()
+
+png(file="drugStorePareto.png")
+pareto(res,TRUE,TRUE,"Drug Store Visits by Department")
+dev.off()
 
