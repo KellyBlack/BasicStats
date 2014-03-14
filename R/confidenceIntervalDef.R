f94fe1bbf2570edc2ec016125a0ffcd83484b131
KellyBlack <kjblack@gmail.com>
2014-03-14 10:51:50 -0400

    Made up new images for the confidence interval.

--- R/confidenceIntervalDef.R
+++ R/confidenceIntervalDef.R
@@ -44,3 +44,34 @@ text(leftBound*0.5,0.02,'-Error')
 
 points(c(0,0),c(yl[1],0.03),type='l')
 dev.off()
+
+# Now do the same for the sample proportion
+png('confidenceIntervalProportion.png')
+plot(x,y,type='l',xlab='sample proportion',ylab='p',main="Error Calculation",axes=FALSE);
+polygon(c(xl,xl[length(xl)],xl[1]),c(yl,yl[1],yl[1]),col='grey')
+polygon(c(xr,xr[length(xr)],xr[1]),c(yr,yr[length(yr)],yr[length(yr)]),col='grey')
+points(c(leftBound,rightBound),c(yl[1],yl[1]),type='l')
+text((leftBound+left)*0.4,0.02,expression(paste(alpha,'/2')),col=2)
+text((rightBound+right)*0.4,0.02,expression(paste(alpha,'/2')),col=2)
+text(0,0.2,expression(paste('1-',alpha)))
+
+axis(1,at=c(left,leftBound,0.0,rightBound,right),
+     pos = c((left+right)/2,0),
+     labels=c('',
+       expression(paste(widehat(p),"-Error")),expression(widehat(p)),
+       expression(paste(widehat(p),"+Error")),''))
+
+axis(2)
+
+
+arrows(rightBound*0.25,0.02,0,0.02,length=0.06,angle=50)
+arrows(rightBound*0.75,0.02,rightBound,0.02,length=0.06,angle=50)
+text(rightBound*0.5,0.02,'Error')
+
+arrows(leftBound*0.25,0.02,0,0.02,length=0.06,angle=50)
+arrows(leftBound*0.75,0.02,leftBound,0.02,length=0.06,angle=50)
+text(leftBound*0.5,0.02,'-Error')
+
+points(c(0,0),c(yl[1],0.03),type='l')
+dev.off()
+
