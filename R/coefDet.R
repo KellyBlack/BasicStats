583289e11322ade081c8f5ff5366978c2cc8fc69
KellyBlack <kjblack@gmail.com>
2014-04-14 11:51:47 -0400

    Updated the talk on the coef. of determination

--- R/coefDet.R
+++ R/coefDet.R
@@ -25,20 +25,37 @@ slope1     = fit$coefficients[[2]]
 intercept1 = fit$coefficients[[1]]
 
 png("coefDetIdea.png")
+
+# Plot the points
 plot(x,y1,cex=2.5,lwd=3,xlab='x',ylab='y',main='Coefficient of Determination')
+
+# Plot the y bar line (horizontal line)
 points(c(x[1],x[4]),c(ybar1,ybar1),type='l',col=4,lwd=2.5,lty=2)
 text(-1.0,ybar1,expression(bar(y)),cex=2,adj=c(0.0,-0.5),col=4)
+
+# Plot the best fit line
 abline(fit1,cex=2,lwd=2.5,col=2)
+
+# Plot the line marking the total deviation
+points(c(x[trial],x[trial])-0.7,c(ybar1,(y1[trial]*.4+ybar1*.6)),type='l',lwd=2.5,lty=1,col=1)
+points(c(x[trial],x[trial])-0.7,c((y1[trial]*.6+ybar1*.4),y1[trial]),type='l',lwd=2.5,lty=1,col=1)
+points(c(x[trial]-.8,x[trial]),c(y1[trial],y1[trial]),type='l',lwd=2.5,lty=2,col=1)
+
 #for(trial in 1:length(x))
 {
   trial = 3
   yVal = slope1*x[trial]+intercept1
   points(c(x[trial],x[trial]),c(y1[trial],yVal),type='l',col=4,lwd=3,lty=1)
-  points(c(x[trial],x[trial]),c(ybar1,yVal),type='l',col=6,lwd=3,lty=3)
+  points(c(x[trial],x[trial]),c(ybar1,yVal),type='l',col=6,lwd=3,lty=1)
   text(x[trial],(ybar1+yVal)*0.5,"Explained\nDeviation",cex=1.25,adj=c(-0.4,0.5),col=6)
   text(x[trial],(y1[trial]+yVal)*0.5,"Unexplained\nDeviation",cex=1.25,adj=c(-0.4,0.5),col=4)
   text(x[trial],(y1[trial]+ybar1)*0.5,"Total Deviation",cex=1.25,adj=c(1.4,0.75),col=9)
 }
+
+# Plot the lines for the explained and unexplained deviation.
+points(c(x[trial],x[trial]+0.7),c(yVal,yVal),type='l',col=6,lwd=2.5,lty=2)
+points(c(x[trial],x[trial]+0.7),c(y1[trial],y1[trial]),type='l',col=4,lwd=2.5,lty=2)
+
 dev.off()
 
 
