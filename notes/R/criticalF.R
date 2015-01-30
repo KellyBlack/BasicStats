7eefe2b6357271a775701e43cd35d849ec8cd3d0
KellyBlack <kjblack@gmail.com>
2015-01-30 12:15:11 -0500

    Changed the R script that generates the critical F values to include more values and change the size of the columns.

--- notes/R/criticalF.R
+++ notes/R/criticalF.R
@@ -7,15 +7,29 @@ printTable = function(df1,dVals,significance,fileName)
     for (d1 in df1) {
 
       row <- sprintf("%3d",d1)
-      for (d2 in dVals) {
-        f   <- sprintf('%05.4f',qf(1-significance,df1=d2,df2=d1))
-        row <- paste(row,'&',f);
-      }
+      for (d2 in dVals)
+          {
+              fVal <- qf(1-significance,df1=d2,df2=d1)
+              if(fVal > 1000.0) {
+                  f   <- sprintf('%7.1f',fVal)
+              }
+              else if (fVal > 100) {
+                  f   <- sprintf('%6.2f',fVal)
+              }
+              else if (fVal > 10) {
+                  f   <- sprintf('%5.3f',fVal)
+              }
+              else {
+                  f   <- sprintf('%05.4f',fVal)
+              }
+
+              row <- paste(row,'&',f);
+          }
       lupe <- lupe + 1
       if(lupe == length(dVals)){
-           cat(paste(row,"\\\\[12pt] \\arrayrulecolor{light-gray}\\hline\\arrayrulecolor{black} "),"\n",file=fileName,append=T);
+           cat(paste(row,"\\\\[2pt] \\arrayrulecolor{light-gray}\\hline\\arrayrulecolor{black} "),"\n",file=fileName,append=T);
          } else {
-           cat(paste(row,"\\\\[5pt] \\arrayrulecolor{light-gray}\\hline\\arrayrulecolor{black} "),"\n",file=fileName,append=T);
+           cat(paste(row,"\\\\[2pt] \\arrayrulecolor{light-gray}\\hline\\arrayrulecolor{black} "),"\n",file=fileName,append=T);
          }
     }
   }
@@ -32,15 +46,16 @@ cat("\\documentclass{article}\n",
     "\\topmargin=-1.2in\n",
     "\\textheight=10.0in\n",
     "\\definecolor{light-gray}{gray}{0.7}",
-    "\\begin{document}\n"
-    ,file=fileName,append=F)
+    "\\begin{document}\n",
+    "\\rowcolors{2}{gray!25}{white}\n",
+    file=fileName,append=F)
 
 for(significance in c(0.10,0.05,0.02,0.01))
     {
         cat("Approximation of the critical values for the $F$-distribution for $\\alpha=$",
             significance,
             ". \n",
-            "\\includegraphics[height=3.0cm]{../img/FDistribution}\n\n",
+            "\\includegraphics[height=2.0cm]{../img/FDistribution}\n\n",
             "{\\fontencoding{T1}\n",
             "\\fontfamily{pcr}\n",
             "\\fontseries{m}\n",
@@ -50,11 +65,13 @@ for(significance in c(0.10,0.05,0.02,0.01))
             ,file=fileName,append=T)
 
 
-        df1 <- c(1:10,12,15,20) 
-        df2 <- c(1-30,40,60,120)
-        row <- paste(rep("l",length(df1)),collapse="",sep="")
-        row <- paste("\\begin{tabular}{l|",row,"}",collapse='',sep="")
+        df1 <- c(1:15,20) 
+        df2 <- c(1:40,10*(5:10),120)
+        criticalP <- c(0.25,0.20,0.15,0.1,0.05,0.025,0.020,0.01,0.005,0.0025,0.001,5e-04)
+        row <- paste("m{18pt}","*{",length(df1)-1,"}{m{18pt}}",collapse="",sep="")
+        row <- paste("\\begin{tabular}{|m{8pt}|",row,"}\\hline",collapse='',sep="")
         cat(row,"\n",file=fileName,append=T);
+
 
         row <- "df2  & df1 "
         for (degree in df1) {
@@ -62,21 +79,14 @@ for(significance in c(0.10,0.05,0.02,0.01))
         }
         row <- gsub("& $","",row,perl=TRUE)
 
-        cat(row,"\\\\","\n",file=fileName,append=T);
-        for (df in 1:8)
+        cat(row,"\\\\","\\hline \n",file=fileName,append=T);
+        df <- 1
+        while(df <= length(df2))
             {
-                printTable((5*df-4):(5*df),df1,significance,fileName)
-                cat("\\\\","\n",file=fileName,append=T);
+                nextdf <- min(df + 5,length(df2));
+                printTable(df2[df:nextdf],df1,significance,fileName)
+                df <- nextdf+1
             }
-        printTable(c(50,60,70,80,90),df1,significance,fileName)
-        cat("\\\\","\n",file=fileName,append=T);
-        printTable(c(100,200,300,400,1000),df1,significance,fileName)
-                                        #row = "$\\infty$ "
-                                        #for (p in df1) {
-                                        #  z = sprintf('%05.4f',-qnorm(p))
-                                        #  row = paste(row,'&',z);
-                                        #}
-                                        #cat(paste(row,"\\\\[5pt] \\arrayrulecolor{light-gray}\\hline\\arrayrulecolor{black} "),"\n",file=fileName,append=T);
         cat("\\end{tabular}}\n",file=fileName,append=T);
         cat("\\clearpage\n",file=fileName,append=T);
     }
