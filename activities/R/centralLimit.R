b71b2e8257e2e30154d9ac106bf933fc373895a6
KellyBlack <kjblack@gmail.com>
2015-02-20 08:31:56 -0500

    Cleaned up the plots and corrected errors for the central limit theorem example

--- activities/R/centralLimit.R
+++ activities/R/centralLimit.R
@@ -3,8 +3,8 @@ N <- 200
 trials <- 25;
 cat('Will print out ',trials,' trials\n');
 v <- c(1:N)
-dev <- c(1:trials);
-means <- c(1:trials);
+dev <- double(trials);
+means <- double(trials);
 
 choice = readline("Pick a distribution:\n1-uniform\n2-gamma\n3-binomial\n4-Poisson\n ")
 
@@ -15,11 +15,11 @@ for (i in 1:trials) {
    if(choice=="1") {
      v[j] <- mean(runif(i,0,1));
    } else if (choice=="2") {
-     v[j] <- mean(rgamma(i,6,3))
+     v[j] <- mean(rgamma(i,14,4))
    } else if (choice=="3") {
-     v[j] <- mean(rbinom(i,10,.3))
+     v[j] <- mean(rbinom(i,10,.5))
    } else if (choice=="4") {
-     v[j] <- mean(rpois(i,3))
+     v[j] <- mean(rpois(i,5))
    }
  }
 
@@ -28,31 +28,40 @@ for (i in 1:trials) {
 
 
  if(choice=="1") {
-    hist(v,xlim=c(0,1),ylim=c(0,65),main='200 Random Variables Evenly Distributed')
-    mean = 0.5
+     hist(v,xlim=c(0,1),ylim=c(0,65),
+          main='200 Trials - Evenly Distributed',
+          sub=paste("Sample mean of ",i,"samples per trial"))
+     mean = 0.5
  } else if (choice=="2") {
-   hist(v,xlim=c(0,7),ylim=c(0,75),main='200 Random Variables With a Gamma Distribution')
-   mean = 2
+     hist(v,xlim=c(0,7),ylim=c(0,75),
+          main='200 Trials - With a Gamma Distribution',
+          sub=paste("Sample mean of ",i,"samples per trial"))
+     mean = 2
  } else if (choice=="3") {
-   hist(v,xlim=c(0,10),ylim=c(0,65),main='200 Random Variables With a Binomial Distribution')
-   mean = 3
+     hist(v,xlim=c(0,10),ylim=c(0,65),
+          main='200 Trials - With a Binomial Distribution',
+          sub=paste("Sample mean of ",i,"samples per trial"))
+     mean = 3
  } else if (choice=="4") {
-   hist(v,xlim=c(0,10),ylim=c(0,85),main='200 Random Variables With a Poisson Distribution')
-   mean = 3
+     hist(v,xlim=c(0,10),ylim=c(0,85),
+          main='200 Trials - With a Poisson Distribution',
+          sub=paste("Sample mean of ",i,"samples per trial"))
+     mean = 3
  }
 
  cat (i,' Samples\n')
  #pause()
+
 }
 
 
-cat('Means - ',mean,'\n')
+cat('Sample Means - ',mean,'\n')
 plot(1:trials,means,
-     main='Means of the Trials',
+     main='Sample Means of the Trials',
      ylim=c(mean*1.25,mean*.75),
-     xlab='Trial',ylab='Mean')
+     xlab='Number of Samples',ylab='Sample Mean')
 
-print('Standard Deviations\n');
+print('Sample Standard Deviations\n');
 plot(1:trials,dev,
-     main='Standard Deviations of the Trials',
-     xlab='Trial',ylab='Standard Deviation');
+     main='Sample Standard Deviations of the Trials',
+     xlab='Number of Samples',ylab='Sample Standard Deviation');
