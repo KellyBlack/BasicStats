19f89a2c40e7b905c95e421215d40f9b0c7af2b0
KellyBlack <kjblack@gmail.com>
2015-02-20 08:46:23 -0500

    Cleaned up the plots used in the example to demonstrate the confidence interval

--- notes/R/confidenceInterval.R
+++ notes/R/confidenceInterval.R
@@ -5,15 +5,16 @@ samples = 10;
 x = c();
 yPos = c();
 frame()
-plot.window(xlim=c(-1,1),ylim=c(-4,N))
+op <- par(mar = c(5.1,4.1,4.1,2.1) + 1.0)
+plot.window(xlim=c(-1,1),ylim=c(-4.8,N+0.5))
 axis(3,at=seq(-1,1,by=.1),labels=seq(-1,1,by=.1),outer=FALSE)
-title("Confidence Interval",xlab=paste("Sample mean - n =",samples))
-points(c(0,0),c(-1,N),type="l",lty=1);
+title(expression(paste("Confidence Interval, ",mu,"=0")),xlab=paste("Sample mean of",samples,"samples"))
+points(c(0,0),c(-1,N+2.0),type="l",lty=1);
 #par(ask = TRUE)
 
 for (i in 1:N) {
   x = c(mean(rnorm(samples)),x);
-  yPos = c(N-i,yPos);
+  yPos = c(N-i+0.5,yPos);
   points(x[1],yPos[1])
   pause = readline("Hit enter to continue: ");
 }
@@ -26,7 +27,7 @@ for (p in c(0.8,0.9,0.95,0.98)) {
   pause = readline("Hit enter to continue: ");
   error = qnorm(p+(1.0-p)/2.0)/sqrt(samples)
   color = color + 1
-  text(-0.8,pos,paste("p =",p),col=color);
+  text(-0.8,1.2*pos-0.25,paste("p =",p),col=color);
   pos = pos - 1.3;
   for (i in 1:N) {
     points(c(x[i]-error,x[i]+error),c(yPos[i],yPos[i]),type="l",lty=1,col=color);
