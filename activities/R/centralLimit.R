d7f0e43239fdd5b0107139abbd6585d69691d0d4
KellyBlack <kjblack@gmail.com>
2015-02-20 08:18:56 -0500

    Added options for different distributions in the central limit theorem example.

--- activities/R/centralLimit.R
+++ activities/R/centralLimit.R
@@ -6,29 +6,50 @@ v <- c(1:N)
 dev <- c(1:trials);
 means <- c(1:trials);
 
+choice = readline("Pick a distribution:\n1-uniform\n2-gamma\n3-binomial\n4-Poisson\n ")
+
 par(ask = TRUE)
 
 for (i in 1:trials) {
  for (j in 1:N) {
-    #v[j] <- mean(rpois(i,3))
-    #v[j] <- mean(rbinom(i,5,.3))
-    #v[j] <- mean(rgamma(i,6,3))
-    v[j] <- mean(runif(i,0,1));
+   if(choice=="1") {
+     v[j] <- mean(runif(i,0,1));
+   } else if (choice=="2") {
+     v[j] <- mean(rgamma(i,6,3))
+   } else if (choice=="3") {
+     v[j] <- mean(rbinom(i,10,.3))
+   } else if (choice=="4") {
+     v[j] <- mean(rpois(i,3))
+   }
  }
 
  dev[i] <- sd(v);
  means[i] <- mean(v);
 
- hist(v,xlim=c(0,1),ylim=c(0,65),main='200 Random Variables Evenly Distributed')
+
+ if(choice=="1") {
+    hist(v,xlim=c(0,1),ylim=c(0,65),main='200 Random Variables Evenly Distributed')
+    mean = 0.5
+ } else if (choice=="2") {
+   hist(v,xlim=c(0,7),ylim=c(0,75),main='200 Random Variables With a Gamma Distribution')
+   mean = 2
+ } else if (choice=="3") {
+   hist(v,xlim=c(0,10),ylim=c(0,65),main='200 Random Variables With a Binomial Distribution')
+   mean = 3
+ } else if (choice=="4") {
+   hist(v,xlim=c(0,10),ylim=c(0,85),main='200 Random Variables With a Poisson Distribution')
+   mean = 3
+ }
+
  cat (i,' Samples\n')
  #pause()
 }
 
 
-print('Means\n')
+cat('Means - ',mean,'\n')
 plot(1:trials,means,
      main='Means of the Trials',
-     ylim=c(.25,.75),
+     ylim=c(mean*1.25,mean*.75),
      xlab='Trial',ylab='Mean')
 
 print('Standard Deviations\n');
